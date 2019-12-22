49d3d162eae20ec84294e3bf8236c3f0780e5edf
knokko <knokogator@hotmail.com>
2019-12-22 12:20:29 +0100

    The 1.15.1 commit

--- CorePlugin/src/nl/knokko/core/plugin/entity/EntityDamageHelper.java
+++ CorePlugin/src/nl/knokko/core/plugin/entity/EntityDamageHelper.java
@@ -1,11 +1,11 @@
 package nl.knokko.core.plugin.entity;
 
-import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_14_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftEntity;
 import org.bukkit.entity.Entity;
 
-import net.minecraft.server.v1_14_R1.EntityDamageSourceIndirect;
-import net.minecraft.server.v1_14_R1.EntitySmallFireball;
+import net.minecraft.server.v1_15_R1.EntityDamageSourceIndirect;
+import net.minecraft.server.v1_15_R1.EntitySmallFireball;
 
 public class EntityDamageHelper {
 
