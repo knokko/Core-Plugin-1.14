accb7158c4c6fdf6926c164a9cb2aa0a2271183c
knokko <knokogator@hotmail.com>
2021-05-02 12:39:35 +0200

    Update a minor comment

--- CorePlugin/src/nl/knokko/core/plugin/block/MushroomBlocks.java
+++ CorePlugin/src/nl/knokko/core/plugin/block/MushroomBlocks.java
@@ -9,7 +9,7 @@ import org.bukkit.block.data.MultipleFacing;
 public class MushroomBlocks {
 	
 	public static boolean areEnabled() {
-		// Custom mushroom blocks are enabled in mc 1.14
+		// Custom mushroom blocks are enabled in mc 1.13
 		return true;
 	}
 
